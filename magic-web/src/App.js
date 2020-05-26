e3df7c7a8d2290dc34f611ac78729d7b55fe8be0
Matt-T-Pham <34947766+Matt-T-Pham@users.noreply.github.com>
2020-05-25 22:44:15 -0600

    Magic web
    Magic Web

---
+++ magic-web/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import HomepageImage from './component/HomepageImage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomepageImage />
+        <p>
+          Hello World
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
